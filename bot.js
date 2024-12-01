c8ce4fcf4682e6d792c9e17acb399c8d09d1adb9
Ajay <mailtoajay7007@gmail.com>
2024-12-01 20:51:25 +0530

    consistency commit

--- bot.js
+++ bot.js
@@ -3,23 +3,22 @@ const { ActivityHandler, MessageFactory } = require('botbuilder');
 class EchoBot extends ActivityHandler {
     constructor() {
         super();
-        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
-        this.onMessage(async (context, next) => {
+                this.onMessage(async (context, next) => {
             const replyText = `Echo: ${ context.activity.text }`;
             await context.sendActivity(MessageFactory.text(replyText, replyText));
-            // By calling next() you ensure that the next BotHandler is run.
+           
             await next();
         });
 
         this.onMembersAdded(async (context, next) => {
             const membersAdded = context.activity.membersAdded;
-            const welcomeText = 'Hello and welcome!';
+            const welcomeText = 'Hello and welcome! This is the Echo Bot. Type anything to get started.';
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
                     await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
                 }
             }
-            // By calling next() you ensure that the next BotHandler is run.
+            
             await next();
         });
     }
